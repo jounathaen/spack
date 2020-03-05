b980d1ad8e586124b5b938d7896e0bd652127d53
Jonathan Klimt <jonathan.klimt@rwth-aachen.de>
2020-03-05 12:02:50 +0100

    Updated neovim package with new versions and checksums

--- var/spack/repos/builtin/packages/neovim/package.py
+++ var/spack/repos/builtin/packages/neovim/package.py
@@ -11,8 +11,15 @@ class Neovim(CMakePackage):
     """NeoVim: the future of vim"""
 
     homepage = "http://neovim.io"
-    url      = "https://github.com/neovim/neovim/archive/v0.3.4.tar.gz"
+    url      = "https://github.com/neovim/neovim/archive/v0.4.3.tar.gz"
 
+    version('0.4.3', sha256='2354a60af47a61f54ad1fc115587af8db96dfbe8c8955c516af591d398134a2d')
+    version('0.4.2', sha256='bf701dfaa456d0e82f7c3fb885861af1282438cc9622853ca76b22d90d0a2516')
+    version('0.4.1', sha256='3148c0909761f36eb9864ddbd228bd2aa0a10016e11034bb764f280c31fa32ab')
+    version('0.4.0', sha256='7d2efa5fd85a62170a3184eba5a405970e5b0bb9ed2b1c38409d45cdde4c5160')
+    version('0.3.8', sha256='f0b0774e9a4f576058a76412b88405a56f984c4c261ab977e379e23e8f10ae34')
+    version('0.3.7', sha256='4ebbcaf2a35af08cf7e04760c69241d233d0b6890344905fa46f053120eae450')
+    version('0.3.6', sha256='e01ff99ac4eca427394903956238306eda9f39fbac9f24e70e9f0f2da434e39d')
     version('0.3.4', sha256='a641108bdebfaf319844ed46b1bf35d6f7c30ef5aeadeb29ba06e19c3274bc0e')
     version('0.3.1', sha256='bc5e392d4c076407906ccecbc283e1a44b7832c2f486cad81aa04cc29973ad22')
     version('0.3.0', sha256='f7acb61b16d3f521907d99c486b7a9f1e505e8b2a18c9ef69a6d7f18f29f74b8')
